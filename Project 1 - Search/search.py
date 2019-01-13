ee1f368385804ae6679ed1190e24df3d02420e30
BigEgg <cd_bigegg@hotmail.com>
2019-01-12 22:48:20 -0800

    Add Depth First Search algorithm

--- Project 1 - Search/search.py
+++ Project 1 - Search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -87,7 +87,24 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    stack = util.Stack()
+    stack.push((problem.getStartState(), []))
+
+    visited = list()
+    visited.append(problem.getStartState())
+
+    while not stack.isEmpty():
+        currentState, steps = stack.pop()
+        if problem.isGoalState(currentState):
+            return steps
+
+        for state, action, cost in problem.getSuccessors(currentState):
+            if state in visited:
+                continue
+            stack.push((state, steps + [ action ]))
+            visited.append(state)
+
+    return []
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
