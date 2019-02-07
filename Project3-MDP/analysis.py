9eb5660fb130607b265dd87197e23f269775dc9a
BigEgg <cd_bigegg@hotmail.com>
2019-02-07 08:51:53 +0800

    #16 Add solutionf for Assignment 3 - Question 3 - Policies

--- Project3-MDP/analysis.py
+++ Project3-MDP/analysis.py
@@ -25,37 +25,42 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the close exit (+1), risking the cliff (-10)
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the close exit (+1), but avoiding the cliff (-10)
+    answerDiscount = 0.1
+    answerNoise = 0.1
+    answerLivingReward = 0.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the distant exit (+10), risking the cliff (-10)
+    answerDiscount = 0.99
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the distant exit (+10), avoiding the cliff (-10)
+    answerDiscount = 0.9
+    answerNoise = 0.1
+    answerLivingReward = 0.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Avoid both exits and the cliff (so an episode should never terminate)
+    answerDiscount = 0
+    answerNoise = 0
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
