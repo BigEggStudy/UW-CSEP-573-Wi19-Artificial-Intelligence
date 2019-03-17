1dacd8e3bb34c55451b9b33d2176e12b3278e575
BigEgg <cd_bigegg@hotmail.com>
2019-03-16 22:08:02 -0700

    #38 Add And-Or Tree nodes

--- Assignment6-AEMS/aems.py
+++ Assignment6-AEMS/aems.py
@@ -47,3 +47,40 @@ class AEMS2(OnlineSolver):
 add any data structure, code, etc you want
 We recommend to have a super class of Node and two subclasses of AndNode and OrNode
 """
+
+class Node(object):
+    def __init__(self, children = [], parent = None):
+        self.lowerBound = float('-inf')
+        self.upperBound = float('inf')
+        self.children = children
+        self.parent = parent
+
+    def getError(self):
+        return self.upperBound - self.lowerBound
+
+class AndNode(Node):
+    def __init__(self, action, discount, reward, children = [], probabilities = [], parent = None):
+        super(AndNode, self).__init__(children, parent)
+        self.action = action
+        self.discount = discount
+        self.reward = reward
+
+        if sum(probabilities) < 1:
+            probabilities = probabilities / np.sum(probabilities)
+        self.probabilities = probabilities
+
+    def backtrack(self):
+        self.lowerBound = self.reward + self.discount * sum([self.probabilities[index] * child.lowerBound for index, child in enumerate(self.children)])
+        self.upperBound = self.reward + self.discount * sum([self.probabilities[index] * child.upperBound for index, child in enumerate(self.children)])
+
+class OrNode(Node):
+    def __init__(self, belief, lb_solver, ub_solver, probability, children = [], parent = None):
+        super(OrNode, self).__init__(children, parent)
+        self.belief = belief
+        self.lowerBound = lb_solver.getValue(self.belief)
+        self.upperBound = ub_solver.getValue(self.belief)
+        self.probability = probability
+
+    def backtrack(self):
+        self.lowerBound = max([child.lowerBound for child in self.children])
+        self.upperBound = max([child.upperBound for child in self.children])
